80e1bb111e9c58f581c608a2a119dc2610806999
hariprasath <hariprasathmuruganantham@gmail.com>
2026-05-19 17:52:10 +0530

    1st commit

---
+++ tests/demo.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demoapps.qspiders.com/ui?scenario=1');
+  await page.getByRole('textbox', { name: 'Name' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('hariprasath');
+  await page.getByRole('textbox', { name: 'Email Id' }).click();
+  await page.getByRole('textbox', { name: 'Email Id' }).fill('hari@gmail.com');
+});
